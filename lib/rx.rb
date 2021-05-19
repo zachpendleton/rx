137d3d93f017f4f5e443800bc0e513551d56a3fe
Zach Pendleton <zachpendleton@gmail.com>
2021-05-19 15:23:00 +0200

    initial commit

---
+++ lib/rx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rx/version"
+
+module Rx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rx/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rx
+  VERSION = "0.1.0"
+end
