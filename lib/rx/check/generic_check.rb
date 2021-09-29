1b43bd16fed0105cc9e2a509ca094927469d3675
Zach Pendleton <zachpendleton@gmail.com>
2021-09-29 09:28:47 +0200

    add optional timeout to generic check

--- lib/rx/check/generic_check.rb
+++ lib/rx/check/generic_check.rb
@@ -1,16 +1,19 @@
+require "timeout"
+
 module Rx
   module Check
     class GenericCheck
-      attr_reader :name
+      attr_reader :name, :timeout
 
-      def initialize(callable, name = "generic")
+      def initialize(callable, name = "generic", timeout = 0)
         @callable = callable
         @name = name
+        @timeout = timeout
       end
 
       def check
         Result.from(name) do
-          callable.call
+          Timeout::timeout(timeout) { callable.call }
         end
       end
 
